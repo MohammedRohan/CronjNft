adf5cbe826b1fd43251ad7f2005c34ed05d12b71
rishab-suresh <56022095+rishab-suresh@users.noreply.github.com>
2022-05-02 04:45:24 +0530

    Add files via upload

---
+++ header/header.jsx
@@ -0,0 +1,22 @@
+import "./header.scss"
+
+const Header =  () => {
+     return (
+      <div className="header">
+        <div className="left">
+            <span className="logo">LOGO</span>
+          
+        </div>
+        
+        <div className="right">
+          <div className="profile">
+              <img src="/Header/assets/user.png" alt="" />
+            </div>
+            
+          
+            
+         
+        </div>
+      </div>
+     );
+  };
